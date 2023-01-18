3afd149839515df1efb490d9c3cc14843a48d432
Sean Misra <seanmisra30@gmail.com>
2023-01-18 13:56:27 -0500

    Fix typo

--- src/app/menu/menu.service.spec.ts
+++ src/app/menu/menu.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MenuService } from './menu-service.service';
+import { MenuService } from './menu.service';
 
 describe('MenuServiceService', () => {
   let service: MenuService;
